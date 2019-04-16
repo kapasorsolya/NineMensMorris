0b75ec2cb20cd8c5bf38118007c669e63f2208bc
Kapas Orsolya <kapas.orsolya@yahoo.com>
2019-04-16 12:45:01 +0300

    corrected project structure

---
+++ NineMensMorrisSolution/src/Models/SimpleMoveEvaluationFunction.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Orsolya
+ */
+public class SimpleMoveEvaluationFunction implements MoveEvaluationFunction {
+	@Override
+	public int evaluate(BoardState boardState, Move move) {
+		if (boardState.doesPieceCompleteMill(move.getFromPosition(), move.getToPosition(), boardState.getCurrentPlayer())) {
+			return 9;
+		}
+		
+		if (boardState.doesPieceCompleteMill(move.getFromPosition(), move.getToPosition(), boardState.getOtherPlayer())) {
+			for (int neighbour : BoardState.POSITION_TO_NEIGHBOURS.get(move.getToPosition())) {
+				if (boardState.getPositionState(neighbour) == boardState.getOtherPlayer() + 1) {
+					return 8;
+				}
+			}
+			
+			return 4;
+		}
+		
+		if (boardState.doesPieceCompleteMill(-1, move.getFromPosition(), boardState.getOtherPlayer())) {
+			for (int neighbour : BoardState.POSITION_TO_NEIGHBOURS.get(move.getFromPosition())) {
+				if (boardState.getPositionState(neighbour) == boardState.getOtherPlayer() + 1) {
+					return -2;
+				}
+			}
+			
+			return -1;
+		}
+
+		return 0;
+	}
+}
